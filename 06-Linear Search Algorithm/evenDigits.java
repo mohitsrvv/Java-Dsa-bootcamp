148205a46d185cfce93a78b92d84b417589ce77f
l0rdluc1f3r <luc1f3rrr@protonmail.ch>
2022-02-28 21:49:23 +0530

    second commit

---
+++ 06-Linear Search Algorithm/evenDigits.java
@@ -0,0 +1,34 @@
+/*Given an array nums of integers, return how many of them contain an even number of digits.
+
+Example 1:
+
+Input: nums = [12,345,2,6,7896]
+Output: 2
+Explanation: 
+12 contains 2 digits (even number of digits). 
+345 contains 3 digits (odd number of digits). 
+2 contains 1 digit (odd number of digits). 
+6 contains 1 digit (odd number of digits). 
+7896 contains 4 digits (even number of digits). 
+Therefore only 12 and 7896 contain an even number of digits. */
+
+
+public class evenDigits {
+    public static void main(String[] args){
+
+    }
+    static int findNumbers(int[] nums){
+        int count = 0;
+        for(int num : nums){
+            if(even(num)){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    // function to check whether a number contains even digits or not
+    static boolean even(int num){
+        
+    }
+}
