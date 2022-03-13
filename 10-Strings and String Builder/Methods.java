a705f7782f7472b59719f594a8cf3a0101a3e2b7
l0rdluc1f3r <luc1f3rrr@protonmail.ch>
2022-03-13 10:46:56 +0530

    second commit

---
+++ 10-Strings and String Builder/Methods.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class Methods {
+    public static void main(String[] args){
+        String name = "The iron man begins";
+        System.out.println(Arrays.toString(name.toCharArray()));
+    }
+}
---
+++ 10-Strings and String Builder/Performance.java
@@ -0,0 +1,9 @@
+public class Performance {
+    public static void main(String[] args){
+        String series = "";
+        for (int i = 0; i < 26; i++) {
+            char ch = (char)('a' + i);
+            System.out.print(ch + " ");
+        }
+    }
+}
