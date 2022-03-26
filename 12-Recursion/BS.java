457ea4d5d838b0f7ae7effb75b5848b6d22e2842
l0rdluc1f3r <luc1f3rrr@protonmail.ch>
2022-03-26 23:18:53 +0530

    second  commit

--- 12-Recursion/BS.java
+++ 12-Recursion/BS.java
@@ -1,6 +1,8 @@
 public class BS {
     public static void main(String[] args){
-
+        int[] arr = {1,2,3,4,55,66,78};
+        int target = 4;
+        System.out.println(search(arr, target, 0, arr.length - 1));
     }
 
     static int search(int[] arr, int target, int s, int e){
@@ -9,5 +11,15 @@ public class BS {
         }
 
         int m = s + (e - s) / 2;
+
+        if(arr[m] == target){
+            return m;
+        }
+
+        if(target < arr[m]){
+            return search(arr, target, s, m-1);
+        }
+
+        return search(arr, target, m+1, e);
     }
 }
